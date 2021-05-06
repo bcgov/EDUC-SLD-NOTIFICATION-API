8a2cbc690ed2a78c13900a839cf1cb75ee36eca1
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-05-06 16:31:45 -0700

    initial commit

--- api/src/main/java/ca/bc/gov/educ/api/sld/notification/SldNotificationApiResourceApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/sld/notification/SldNotificationApiResourceApplication.java
@@ -1,10 +1,15 @@
 package ca.bc.gov.educ.api.sld.notification;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +17,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * The type Sld notification api resource application.
@@ -60,5 +66,18 @@ public class SldNotificationApiResourceApplication {
         .anyRequest().authenticated().and()
         .oauth2ResourceServer().jwt();
     }
+  }
+
+  /**
+   * Lock provider lock provider.
+   *
+   * @param jdbcTemplate       the jdbc template
+   * @param transactionManager the transaction manager
+   * @return the lock provider
+   */
+  @Bean
+  @Autowired
+  public LockProvider lockProvider(final JdbcTemplate jdbcTemplate, final PlatformTransactionManager transactionManager) {
+    return new JdbcTemplateLockProvider(jdbcTemplate, transactionManager, "SLD_NOTIFICATION_SHEDLOCK");
   }
 }
